fd9ffcc3fabc5b6a1753d28c0cf63f414de0b51e
alvdelci <delcimarioalvesdev@gmail.com>
2025-04-24 11:14:37 -0300

    add jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest', // converte todos os test de typescript para javascript
+    modulenameMapper: {
+      '@/(.*)': '<rootDir>/src/$1' // para o jest entender os imports com @
+    }
+  }
+}
